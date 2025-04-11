c89ff6148c6b2ffc504c0afd7eecdf3c00af7380
DPMods <53286132+DPModsYT@users.noreply.github.com>
2025-04-12 00:30:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
-# config.py
+import os
 
-BOT_TOKEN = "YOUR_BOT_TOKEN"
-FORCE_SUB_CHANNEL = "dp_mods"  # without '@'
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = 2531382
+API_HASH = "12a15ce8f731edb4e9c4b6df278916d9"
+
+FORCE_SUB_CHANNEL = "dp_mods"
 ADMIN_USERNAME = "dpmods"
